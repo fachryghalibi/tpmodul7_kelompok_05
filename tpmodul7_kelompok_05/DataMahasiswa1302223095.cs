3fcfc86bcfb89762da542674192e4d612e1e32c0
fadlan kamal <muhfadlan.kamal@gmail.com>
2024-04-01 02:18:15 +0700

    menambahkan class KuliahMahasiswa

--- tpmodul7_kelompok_05/DataMahasiswa1302223095.cs
+++ tpmodul7_kelompok_05/DataMahasiswa1302223095.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace tpmodul7_kelompok_05
 {
-    internal class Class1
+    internal class Datamahasiswa_1302223095
     {
+        public class Nama
+        {
+            public String depan { get; set; }
+            public String belakang { get; set; }
+        }
+        public Nama nama { get; set; }
+        public int nim { get; set; }
+        public String fakultas { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = "C:\\Users\\fadla\\codingan\\SEM 4\\KPL\\tpmodul7_kelompok_5\\tpmodul7_kelompok_05\\tpmodul7_kelompok_05\\tp7_1_1302223095.json";
+
+            string jsonText = File.ReadAllText(filePath);
+
+            Datamahasiswa_1302223095 dataMahasiswa = JsonSerializer.Deserialize<Datamahasiswa_1302223095>(jsonText);
+
+            Console.WriteLine($"Nama {dataMahasiswa.nama.depan} {dataMahasiswa.nama.belakang} dengan NIM {dataMahasiswa.nim} dari Fakultas {dataMahasiswa.fakultas}");
+        }
+
     }
 }
--- tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_05;
+using static tpmodul7_kelompok_05.KuliahMahasiswa_1302223095;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Datamahasiswa_1302223095 mhs = new Datamahasiswa_1302223095();
+
+        mhs.ReadJSON();
+
+        KuliahMahasiswa1302223095 kuliahMahasiswa = new KuliahMahasiswa1302223095();
+        kuliahMahasiswa.ReadJson2();
+
+    }
+}
