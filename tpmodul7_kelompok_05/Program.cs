c73e697a0d33e259475195f265a68974d00a38cb
Fachruddin Ghalibi <fachruddinghalibi@gmail.com>
2024-04-01 01:13:44 +0700

    menambahkan class KuliahMahasiswa

--- tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tpmodul7_kelompok_05;
+using static tpmodul7_kelompok_05.KuliahMahasiswa_1302223107;
 
 class Program
 {
@@ -7,5 +8,9 @@ class Program
         Datamahasiswa_1302223107 mhs = new Datamahasiswa_1302223107();
 
         mhs.ReadJSON();
+
+        KuliahMahasiswa_1302220104 kuliahMahasiswa = new KuliahMahasiswa_1302220104();
+        kuliahMahasiswa.ReadJson2();
+
     }
 }
