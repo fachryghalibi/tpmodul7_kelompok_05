26668ce9e5da351395ac5ff65395a0ff461491ee
Fachruddin Ghalibi <fachruddinghalibi@gmail.com>
2024-04-01 01:06:40 +0700

    menambahkan class Datamahasiswa

---
+++ tpmodul7_kelompok_05/Datamahasiswa_1302223107.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_05
+{
+    internal class Datamahasiswa_1302223107
+    {
+        public class Nama
+        {
+            public String depan { get; set; }
+            public String belakang { get; set; }
+        }
+        public Nama nama { get; set; }
+        public int nim { get; set; }
+        public String fakultas { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = "D:\\Fachry\\Kuliah\\Praktikum KPL\\tpmodul7_kelompok_05\\tpmodul7_kelompok_05\\tp7_1_1302223107.json";
+
+            string jsonText = File.ReadAllText(filePath);
+
+            Datamahasiswa_1302223107 dataMahasiswa = JsonSerializer.Deserialize<Datamahasiswa_1302223107>(jsonText);
+
+            Console.WriteLine($"Nama {dataMahasiswa.nama.depan} {dataMahasiswa.nama.belakang} dengan NIM {dataMahasiswa.nim} dari Fakultas {dataMahasiswa.fakultas}");
+        }
+
+    }
+}
--- tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Datamahasiswa_1302223107 mhs = new Datamahasiswa_1302223107();
+
+        mhs.ReadJSON();
+    }
+}
